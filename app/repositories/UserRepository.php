79aa987af8e50b227839b397ef693c85d5784d37
Lana <chewygum56@gmail.com>
2023-01-04 12:44:25 +0700

    add user repository

---
+++ app/repositories/UserRepository.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Core\Database;
+use App\Models\User;
+
+class UserRepository
+{
+    private $db;
+    private const db_name = 'user';
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function create(User $user): bool
+    {
+        $this->db->query("INSERT INTO user VALUES ('', :email, :password, :name, :role");
+        $this->db->bind('email', $user->email);
+        $this->db->bind('password', $user->password);
+        $this->db->bind('name', $user->name);
+        $this->db->bind('role', $user->role);
+
+        return$this->db->execute();
+    }
+
+    public function getAll(): array
+    {
+        $this->db->query('SELECT * FROM ' . self::db_name . ' WHERE deleted_at is null');
+        return $this->db->fetchAll();
+    }
+
+    public function find(string $key, string $value): User
+    {
+        $this->db->query('SELECT * FROM ' . self::db_name . ' WHERE ' . $key . ' = :' . $key . ' AND deleted_at is null');
+        $this->db->bind($key, $value);
+        $data = $this->db->fetch();
+
+        $user = new User;
+        $user->id = $data['id'];
+        $user->email = $data['email'];
+        $user->password = $data['password'];
+        $user->name = $data['name'];
+        $user->role = $data['role'];
+        $user->created_at = $data['created_at'];
+        $user->deleted_at = $data['deleted_at'];
+
+        return $user;
+    }
+
+    public function update(User $user) : bool
+    {
+        $this->db->query('UPDATE user SET email = :email, password = :password, name = :name, role = :role WHERE id = :id');
+
+        $this->db->bind('id', $user->id);
+        $this->db->bind('email', $user->email);
+        $this->db->bind('password', $user->password);
+        $this->db->bind('name', $user->name);
+        $this->db->bind('role', $user->role);
+
+        return $this->db->execute();
+    }
+
+    public function delete($id) : bool
+    {
+        $this->db->query("UPDATE user SET deleted_at = :deleted_at WHERE id = :id");
+
+        $this->db->bind('id', $id);
+        $this->db->bind('deleted_at', date('y-m-d h:m:s', time()));
+
+        return $this->db->execute();
+    }
+}
