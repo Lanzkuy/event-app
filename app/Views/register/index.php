164cea2381dea05a749100c8892da6a9bf8187d2
Lana <59499676+Lanzkuy@users.noreply.github.com>
2023-01-05 12:36:22 +0700

    remove app\Views directory

--- app/Views/register/index.php
+++
@@ -1,7 +0,0 @@
-<form action="" method="post">
-    <input type="text" placeholder="Email"><br>
-    <input type="text" placeholder="Name"><br>
-    <input type="password" placeholder="Password"><br>
-    <input type="submit" value="Register">
-    <a href="login">Login</a>
-</form>
