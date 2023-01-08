487be5ea486e9011e4c388b146ba579f3c5b2b3f
izzanka <izzancoc.46@gmail.com>
2023-01-08 13:37:29 +0700

    adding model event

---
+++ app/models/Event.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+class Event
+{
+    public int $id;
+    public int $category_id;
+    public int $user_id;
+    public string $title;
+    public string $description;
+    public string $image;
+    public string $location;
+    public string $start_datetime;
+    public string $end_datetime;
+    public string $status;
+    public string $created_at;
+    public string $deleted_at;
+}
