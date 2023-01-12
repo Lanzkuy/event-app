8a1965c8bc7770e3d6b462404740d4790f115286
Lana <chewygum56@gmail.com>
2023-01-12 23:28:09 +0700

    add flasher class

---
+++ app/core/Flasher.php
@@ -0,0 +1,23 @@
+<?php
+
+class Flasher
+{
+    public static function setFlash(string $message, string $type)
+    {
+        $_SESSION['alert'] = [
+            'message' => $message,
+            'type' => $type
+        ];
+    }
+
+    public static function flash()
+    {
+        if(isset($_SESSION['alert'])) {
+            echo '<div class="alert alert-' . $_SESSION['alert']['type'] . ' alert-dismissible fade show" role="alert">
+                    '. $_SESSION['alert']['message'] .'
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+            unset($_SESSION['alert']);
+        }
+    }
+}
