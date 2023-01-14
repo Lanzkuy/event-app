81a16c5fcf77e049c3803ca6d3f14fa4eea84c75
Irfansatriatama <irfansatriatama@gmail.com>
2023-01-14 11:05:03 +0700

    fe home sketch

---
+++ view/home.php
@@ -0,0 +1,87 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/a91588ba6f.js" crossorigin="anonymous"></script>
+    <link href="style.css" rel="stylesheet">
+    <title>Event App</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-primary auto">
+        <div class="container-fluid">
+            <div class="nav-search">
+                <a class="navbar-brand" href="#">EVENT WEB</a>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button type="button" class="btn btn-dark">Search</button>
+                </form>
+            </div>             
+            <div class="nav-r">
+                <a class="nav-link" href="#">Buat Event</a>
+                <a class="nav-link" href="#">LOGIN/REGISTER</a>
+            </div>
+        </div>
+    </nav>
+    <section>
+        <div>
+            
+        </div>
+    </section>
+    <section id="banner" class="">
+        <h2>Welcome to Event Website</h2>
+        <h4>Join or Make your event now!</h4>
+    </section>
+
+    <section id="col-trend" class="section-p1">
+        <h4>Trend Event</h4>
+        <div class="pro-container">
+            <div class="pro">
+                <img src="img/f1.jpg" alt="">
+                <div class="des">
+                    <span>FAE</span>
+                    <h5>Abstract T-Shirt</h5>                    
+                    <h4>$59</h4>
+                </div>
+            </div>
+            <div class="pro">
+                <img src="img/f2.jpg" alt="">
+                <div class="des">
+                    <span>FAE</span>
+                    <h5>Jasmine T-Shirt</h5>                    
+                    <h4>$59</h4>
+                </div>
+            </div>
+            <div class="pro">
+                <img src="img/f3.jpg" alt="">
+                <div class="des">
+                    <span>FAE</span>
+                    <h5>Rose T-Shirt</h5>                    
+                    <h4>$59</h4>
+                </div>
+            </div>
+            <div class="pro">
+                <img src="img/f4.jpg" alt="">
+                <div class="des">
+                    <span>FAE</span>
+                    <h5>Sakura T-Shirt</h5>                    
+                    <h4>$59</h4>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
