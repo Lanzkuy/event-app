0b7de621be74f047a94a267000421fa0adb6bf77
izzanka <izzancoc.46@gmail.com>
2023-01-14 19:03:20 +0700

    add create ticket in view event create

--- app/views/user/event/create.php
+++ app/views/user/event/create.php
@@ -12,7 +12,18 @@
     <input type="text" name="location" placeholder="location"><br>
     <input type="datetime-local" name="start_datetime" placeholder="start_datetime"><br>
     <input type="datetime-local" name="end_datetime" placeholder="end_datetime"><br>
+    <hr>
+        <input type="text" value="Reguler" disabled><br>
+        <input type="number" name="reguler_price" placeholder="price ticket"><br>
+        <input type="number" name="reguler_stock" placeholder="stock"><br>
+        <input type="text" name="reguler_description" placeholder="desc"><br><br>
 
+        <input type="text" value="VIP" disabled><br>
+        <input type="number" name="vip_price" placeholder="price ticket"><br>
+        <input type="number" name="vip_stock" placeholder="stock"><br>
+        <input type="text" name="vip_description" placeholder="desc"><br><br>
+
+    <hr>
     <input type="submit" value="Create">
 </form>
 <a href="../event">back</a>
