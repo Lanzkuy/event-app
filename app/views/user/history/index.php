c3e0157102ead1f0d1cc8a79c3b95d357347cfb4
izzanka <izzancoc.46@gmail.com>
2023-01-14 19:02:38 +0700

    change view history index to table

--- app/views/user/history/index.php
+++ app/views/user/history/index.php
@@ -2,17 +2,26 @@
 
 <?php if($data['orderDetail']){ ?>
 
-    <?php foreach($data['orderDetail'] as $orderDetails){ ?>
+    <table>
+        <tr>
+            <th>event_title</th>
+            <th>ticket_type</th>
+            <th>ticket_price</th>
+            <th>ticket_qty</th>
+            <th>action</th>
+        </tr>
+        <?php foreach($data['orderDetail'] as $orderDetails){ ?>
         <?php foreach($orderDetails as $orderDetail){ ?>
-            <hr>
-            <?= $orderDetail['event_title'] ?><br>
-            <?= $orderDetail['ticket_type'] ?><br>
-            <?= $orderDetail['ticket_price']?><br>
-            <?= $orderDetail['qty'] ?><br>
-            <a href="">print ticket</a>
-            <hr>
+            <tr>
+                <td><?= $orderDetail['event_title'] ?><br></td>
+                <td><?= $orderDetail['ticket_type'] ?><br></td>
+                <td><?= $orderDetail['ticket_price']?><br></td>
+                <td><?= $orderDetail['qty'] ?><br></td>
+                <td><a href="">print ticket</a></td>
+            </tr>
         <?php } ?>
     <?php } ?>
+    </table>
 
 <?php }else{ ?>
     no data
