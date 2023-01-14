af3331e08aac754ac840e804ffea189f12813859
izzanka <izzancoc.46@gmail.com>
2023-01-14 09:50:26 +0700

    add history controller

---
+++ app/controllers/HistoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers;
+
+use Exception;
+use App\Core\Controller;
+use App\Services\OrderService;
+use App\Services\OrderDetailService;
+
+class HistoryController extends Controller
+{
+    private OrderService $orderService;
+    private OrderDetailService $orderDetailService;
+
+    public function __construct()
+    {
+        $this->orderService = $this->service('Order');
+        $this->orderDetailService = $this->service('OrderDetail');
+    }
+
+    public function index()
+    {
+       $ordersArray = [];
+       $orders = $this->orderService->getOrderByStatus(2);
+
+       $orderDetails = [];
+       foreach($orders as $order){
+            $orderDetails[] = $this->orderDetailService->getOrderDetailByOrderId($order['id']);
+       }
+
+       $data['title'] = 'List Ordered Tickets';
+       $data['orderDetail'] = $orderDetails;
+
+       $this->view('templates/header', $data);
+       $this->view('user/history/index', $data);
+       $this->view('templates/footer');
+
+    }
+        
+}
